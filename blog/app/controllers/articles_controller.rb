167f0ea133b96980c38470b13f5cab6835b8571b
justus <jmbaluka@mfarm.co.ke>
2015-04-16 15:44:00 +0300

    edited index view to allow listing of all availabe articles

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,16 +1,15 @@
 class ArticlesController < ApplicationController
 
-     def index
-     	@articles = Article.all     	
-     end
-
-     def show
-     	@article = Article.find(params[:id])
-     end
-
-     def new
-     	
-     end
+      def index
+           @articles = Article.all
+      end
+ 
+      def show
+        @article = Article.find(params[:id])
+       end
+ 
+      def new
+      end
 
      def create
         @article = Article.new(article_params)
